055c319327f915952009e3658a3070b81de09b23
ccastro01 <alvescastro00cca13@gmail.com>
2021-02-06 12:51:37 -0300

    Creating userController and adding validation

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,16 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { ResponseUserDTO } from './dtos/response-user.dto';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
-    constructor( private userService: UserService ){}
+  constructor(private userService: UserService) {}
 
-    @Post()
-    async createAdminUser(
-        @Body() createUserDto: CreateUserDto,
-    ): Promise<ResponseUserDTO>{
-        const user = await this.userService.
-    }
+  @Post()
+  async createAdminUser(
+    @Body(ValidationPipe) createUserDto: CreateUserDto,
+  ): Promise<ResponseUserDTO> {
+    const user = await this.userService.createAdminUser(createUserDto);
+    return {
+      user,
+      message: 'Administrador cadastrado com sucesso',
+    };
+  }
 }
