8e76eb0d2b2f4b917ac0b688e9eca6bbcd83adfe
ccastro01 <alvescastro00cca13@gmail.com>
2021-02-07 15:30:39 -0300

    integrating Swagger in the API

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  getVersion(): string {
+    return 'CyberAid API v1.0';
   }
 }
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { ResponseUserDTO } from './dtos/response-user.dto';
 import { UserService } from './user.service';
 
+@ApiTags('User')
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
