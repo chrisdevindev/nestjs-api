9bcc53f9311832a3849a23c6bab9fbfc3c765d63
ccastro01 <alvescastro00cca13@gmail.com>
2021-02-09 02:11:08 -0300

    Creating Auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from 'src/modules/user/repositories/user.repository';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        @InjectRepository(UserRepository)
+        private userRepository :UserRepository
+    ){}
+}
