1b94b4558e5d284aa8bd825673a29e13d3d41870
ccastro01 <alvescastro00cca13@gmail.com>
2021-02-09 19:56:36 -0300

    [P2] Creating user signUp service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/entities/user.entity';
+import { CreateUserDto } from 'src/modules/user/dtos/create-user.dto';
+import { UserRole } from 'src/modules/user/enum/user-roles.enum';
 import { UserRepository } from 'src/modules/user/repositories/user.repository';
 
 @Injectable()
@@ -8,4 +11,12 @@ export class AuthService {
         @InjectRepository(UserRepository)
         private userRepository :UserRepository
     ){}
+
+    async signUp(createUserDTO: CreateUserDto): Promise<User>{
+        if(createUserDTO.password != createUserDTO.passwordConfirmation){
+            throw new UnprocessableEntityException('As senhas estão diferentes');
+        }else{
+            return await this.userRepository.createUser(createUserDTO, UserRole.USER);
+        }
+    }
 }
